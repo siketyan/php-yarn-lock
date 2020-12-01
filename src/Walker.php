509c4cd8c770d5bd7141308be7da699ae09e89da
Naoki Ikeguchi <root@siketyan.dev>
2020-12-01 22:49:25 +0900

    ✨ Add parser

---
+++ src/Walker.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siketyan\YarnLock;
+
+class Walker
+{
+    /**
+     * @var string[]
+     */
+    private array $lines;
+
+    private int $cursor = 0;
+
+    /**
+     * @param string[] $lines
+     */
+    public function __construct(
+        array $lines
+    ) {
+        $this->lines = $lines;
+    }
+
+    public function read(): ?string
+    {
+        return $this->lines[$this->cursor] ?? null;
+    }
+
+    public function step(): void
+    {
+        ++$this->cursor;
+    }
+}
